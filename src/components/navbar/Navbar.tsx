a29e91a7b655d5e21b14b4490c59ec198ac7a79a
kauedota <kauedota@gmail.com>
2026-08-18 09:02:28 -0300

    💄 UI: Criação de Rotas

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom"
+
 function Navbar() {
   return (
     <div className="w-full flex justify-center bg-indigo-900 text-white">
       <div className="container flex flex-wrap items-center justify-between py-4 px-4">
         <a href="#" className="text-2xl font-bold">
-          Blog Pessoal
+          <Link to="/" className="text-2xl font-bold">Blog Pessoal</Link>
         </a>
 
         <div className="flex gap-4 text-lg">
@@ -12,6 +14,7 @@ function Navbar() {
           <a href="#" className="hover:text-indigo-300">Temas</a>
           <a href="#" className="hover:text-indigo-300">Cadastrar Usuário</a>
           <a href="#" className="hover:text-indigo-300">Login</a>
+          <Link to="/login" className="hover:underline">Sair</Link>
         </div>
       </div>
     </div>
